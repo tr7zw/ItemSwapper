056e2db31ae102ce6f92d8e5091c4c9d3f53d78d
AdvancedWipe <65723138+AdvancedWipe@users.noreply.github.com>
2022-12-06 21:32:25 +0100

    clean up code in ConfigManager
    * replace System.out statements by logger statements
    * remove code duplication at line 30
    * simplify usage of new String()
    * replace Exception in catch statement by the actual exceptions that could be thrown

--- Shared/src/main/java/dev/tr7zw/itemswapper/ConfigManager.java
+++ Shared/src/main/java/dev/tr7zw/itemswapper/ConfigManager.java
@@ -1,8 +1,8 @@
 package dev.tr7zw.itemswapper;
 
+import com.google.gson.JsonSyntaxException;
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 import com.google.gson.Gson;
@@ -10,53 +10,54 @@ import com.google.gson.GsonBuilder;
 
 public final class ConfigManager {
 
-    private static ConfigManager instance = new ConfigManager();
-    
-    public static ConfigManager getInstance() {
-        return instance;
-    }
-    
     private Config config;
-    private final File settingsFile = new File("config", "itemswapper.json");
+    private static final ConfigManager INSTANCE = new ConfigManager();
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    
+    private final File configFile = new File("config", "itemswapper.json");
+
     private ConfigManager() {
-        if (settingsFile.exists()) {
+        if (configFile.exists()) {
             try {
-                config = gson.fromJson(new String(Files.readAllBytes(settingsFile.toPath()), StandardCharsets.UTF_8),
-                        Config.class);
-            } catch (Exception ex) {
-                System.out.println("Error while loading config! Creating a new one!");
-                ex.printStackTrace();
+                config = gson.fromJson(Files.readString(configFile.toPath()), Config.class);
+            } catch (JsonSyntaxException | IOException exception) {
+                ItemSwapperSharedMod.LOGGER.warn("Error while loading config: " + exception.getMessage());
+                ItemSwapperSharedMod.LOGGER.warn("A new configuration will be created!");
             }
         }
+
         if (config == null) {
-            config = new Config();
-            writeConfig();
+            reset();
         } else {
-            if(ConfigUpgrader.upgradeConfig(config)) {
+            if (ConfigUpgrader.upgradeConfig(config)) {
                 writeConfig(); // Config got modified
             }
         }
     }
-    
+
     public Config getConfig() {
         return config;
     }
-    
+
+    public static ConfigManager getInstance() {
+        return INSTANCE;
+    }
+
     public void reset() {
         config = new Config();
         writeConfig();
     }
-    
+
     public void writeConfig() {
-        if (settingsFile.exists())
-            settingsFile.delete();
+        if (configFile.exists()) {
+            boolean isDeleted = configFile.delete();
+            ItemSwapperSharedMod.LOGGER.debug("Config could be deleted before writing to it: " + isDeleted);
+        }
+
         try {
-            Files.write(settingsFile.toPath(), gson.toJson(config).getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e1) {
-            e1.printStackTrace();
+            Files.writeString(configFile.toPath(), gson.toJson(config));
+        } catch (IOException ioException) {
+            ItemSwapperSharedMod.LOGGER.warn(ioException.getMessage());
         }
     }
-    
+
 }
