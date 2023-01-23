4e3edf6de7afc0444a962fb30e5bd61342efe9ab
tr7zw <tr7zw@live.de>
2023-01-23 19:42:21 +0100

    Add InstrumentItemNameProvider, Add instrument list, late init providers

---
+++ Shared/src/main/java/dev/tr7zw/itemswapper/accessor/InstrumentItemAccess.java
@@ -0,0 +1,17 @@
+package dev.tr7zw.itemswapper.accessor;
+
+import java.util.Optional;
+import java.util.Set;
+
+import net.minecraft.core.Holder;
+import net.minecraft.world.item.Instrument;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public interface InstrumentItemAccess {
+
+    public Set<Item> getItems();
+    
+    public Optional<? extends Holder<Instrument>> getOptionalInstrument(ItemStack itemStack);
+    
+}
