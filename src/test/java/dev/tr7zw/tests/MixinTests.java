84bf092769d7992831ee902c5b090f3c52ef3a6b
tr7zw <tr7zw@live.de>
2024-03-13 00:02:17 +0100

    1.20.2 backport

--- src/test/java/dev/tr7zw/tests/MixinTests.java
+++ src/test/java/dev/tr7zw/tests/MixinTests.java
@@ -1,26 +1,17 @@
 package dev.tr7zw.tests;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
-import java.util.List;
-import java.util.Set;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.objenesis.Objenesis;
 import org.objenesis.ObjenesisStd;
 
-import dev.tr7zw.config.CustomConfigScreen;
-import dev.tr7zw.itemswapper.ItemSwapperMod;
 import net.minecraft.SharedConstants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.MouseHandler;
-import net.minecraft.client.OptionInstance;
 import net.minecraft.client.gui.screens.inventory.ContainerScreen;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.server.IntegratedPlayerList;
-import net.minecraft.locale.Language;
 import net.minecraft.server.Bootstrap;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.item.InstrumentItem;
@@ -46,22 +37,6 @@ public class MixinTests {
         objenesis.newInstance(IntegratedPlayerList.class);
         objenesis.newInstance(RecordItem.class);
         objenesis.newInstance(ServerGamePacketListenerImpl.class);
-    }
-
-    @Test
-    public void langTests() throws Throwable {
-        Language lang = TestUtil.loadDefault("/assets/itemswapper/lang/en_us.json");
-        ItemSwapperMod.instance = new TestMod();
-        CustomConfigScreen screen = (CustomConfigScreen) ItemSwapperMod.instance.createConfigScreen(null);
-        List<OptionInstance<?>> options = TestUtil.bootStrapCustomConfigScreen(screen);
-        assertNotEquals(screen.getTitle().getString(), lang.getOrDefault(screen.getTitle().getString()));
-        for (OptionInstance<?> option : options) {
-            Set<String> keys = TestUtil.getKeys(option, true);
-            for (String key : keys) {
-                System.out.println(key + " " + lang.getOrDefault(key));
-                assertNotEquals(key, lang.getOrDefault(key));
-            }
-        }
     }
 
 }
--- src/test/java/dev/tr7zw/tests/TestMod.java
+++
@@ -1,7 +0,0 @@
-package dev.tr7zw.tests;
-
-import dev.tr7zw.itemswapper.ItemSwapperMod;
-
-public class TestMod extends ItemSwapperMod {
-
-}
