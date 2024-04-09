473a25860b75919c6d15bea561fbca70ca3136c9
tr7zw <tr7zw@live.de>
2024-04-09 09:58:09 +0200

    Pickblock modes fix for Litematica. Fix #203

--- src/main/java/dev/tr7zw/itemswapper/mixin/LitematicaMixin.java
+++ src/main/java/dev/tr7zw/itemswapper/mixin/LitematicaMixin.java
@@ -5,6 +5,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import dev.tr7zw.itemswapper.ItemSwapperSharedMod;
+import dev.tr7zw.itemswapper.config.ConfigManager;
+import dev.tr7zw.itemswapper.config.PickBlockMode;
+import dev.tr7zw.itemswapper.manager.itemgroups.ItemList;
 import dev.tr7zw.itemswapper.util.ItemUtil;
 import fi.dy.masa.litematica.materials.MaterialCache;
 import fi.dy.masa.litematica.util.RayTraceUtils;
@@ -12,6 +16,7 @@ import fi.dy.masa.litematica.util.WorldUtils;
 import fi.dy.masa.litematica.world.SchematicWorldHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -22,8 +27,23 @@ public class LitematicaMixin {
     @Inject(method = "doSchematicWorldPickBlock", at = @At("HEAD"), remap = true, cancellable = true)
     private static void doSchematicWorldPickBlockHook(boolean closest, Minecraft mc,
             CallbackInfoReturnable<Boolean> ci) {
+        if (ConfigManager.getInstance().getConfig().pickblockOnToolsWeapons != PickBlockMode.ALLOW) {
+            ItemList list = ItemSwapperSharedMod.instance.getItemGroupManager()
+                    .getList(mc.player.getMainHandItem().getItem());
+            if (list != null && (list.getId().equals(new ResourceLocation("itemswapper", "v2/weapons"))
+                    || list.getId().equals(new ResourceLocation("itemswapper", "v2/tools")))) {
+                if (ConfigManager.getInstance().getConfig().pickblockOnToolsWeapons == PickBlockMode.PREVENT_ON_TOOL) {
+                    // skip Litematica logic
+                    ci.setReturnValue(true);
+                    ci.cancel();
+                }
+                // else it's VANILLA_ON_TOOL, so just do that
+                return;
+            }
+        }
+        // pickblock from shulker
+        
         BlockPos pos;
-
         pos = RayTraceUtils.getSchematicWorldTraceIfClosest(mc.level, mc.player, 6);
         if (pos != null) {
             Level world = SchematicWorldHandler.getSchematicWorld();
