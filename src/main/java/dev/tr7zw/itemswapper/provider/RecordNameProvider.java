71b454226d4c7c4254d136fc9087ae94d6b98f26
Wolfdv1 <Wolfdv1@github.com>
2024-06-15 16:51:54 +0200

    Record hack... unsure why casting here ((ItemVariantAccess) (Object) Items.MUSIC_DISC_CAT).getAllItemVariants(); is broken, regardless won't effect vanilla records, TODO fix for modded

--- src/main/java/dev/tr7zw/itemswapper/provider/RecordNameProvider.java
+++ src/main/java/dev/tr7zw/itemswapper/provider/RecordNameProvider.java
@@ -12,12 +12,33 @@ import net.minecraft.world.item.Items;
 
 public class RecordNameProvider implements NameProvider {
 
+    private Set<Item> BY_NAME;
+
     @Override
     public Set<Item> getItemHandlers() {
         // TODO late init this, so other mods music discs are loaded?
         //spotless:off
         //#if MC >= 12100
-        return Items.MUSIC_DISC_CAT.getAllItemVariants();
+            BY_NAME.add(Items.MUSIC_DISC_13);
+            BY_NAME.add(Items.MUSIC_DISC_CAT);
+            BY_NAME.add(Items.MUSIC_DISC_BLOCKS);
+            BY_NAME.add(Items.MUSIC_DISC_CHIRP);
+            BY_NAME.add(Items.MUSIC_DISC_CREATOR);
+            BY_NAME.add(Items.MUSIC_DISC_CREATOR_MUSIC_BOX);
+            BY_NAME.add(Items.MUSIC_DISC_FAR);
+            BY_NAME.add(Items.MUSIC_DISC_MALL);
+            BY_NAME.add(Items.MUSIC_DISC_MELLOHI);
+            BY_NAME.add(Items.MUSIC_DISC_STAL);
+            BY_NAME.add(Items.MUSIC_DISC_STRAD);
+            BY_NAME.add(Items.MUSIC_DISC_WARD);
+            BY_NAME.add(Items.MUSIC_DISC_11);
+            BY_NAME.add(Items.MUSIC_DISC_WAIT);
+            BY_NAME.add(Items.MUSIC_DISC_OTHERSIDE);
+            BY_NAME.add(Items.MUSIC_DISC_RELIC);
+            BY_NAME.add(Items.MUSIC_DISC_5);
+            BY_NAME.add(Items.MUSIC_DISC_PIGSTEP);
+            BY_NAME.add(Items.MUSIC_DISC_PRECIPICE);
+            return BY_NAME;
         //#else
         //$$ return ((ItemVariantAccess) (Object) Items.MUSIC_DISC_CAT).getAllItemVariants();
         //#endif
