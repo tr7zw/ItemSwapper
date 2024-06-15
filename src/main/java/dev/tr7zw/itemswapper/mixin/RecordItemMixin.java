b7bcd9357d47b275232a50f3997ec9abe237975d
Wolfdv1 <Wolfdv1@github.com>
2024-06-15 16:57:42 +0200

    fixed rookie error

--- src/main/java/dev/tr7zw/itemswapper/mixin/RecordItemMixin.java
+++ src/main/java/dev/tr7zw/itemswapper/mixin/RecordItemMixin.java
@@ -21,29 +21,29 @@ import net.minecraft.world.item.Items;
 
 @Mixin(Items.class)
 public class RecordItemMixin implements ItemVariantAccess {
-    private Set<Item> BY_NAME;
 
     @Override
     public Set<Item> getAllItemVariants() {
-        BY_NAME.add(Items.MUSIC_DISC_13);
-        BY_NAME.add(Items.MUSIC_DISC_CAT);
-        BY_NAME.add(Items.MUSIC_DISC_BLOCKS);
-        BY_NAME.add(Items.MUSIC_DISC_CHIRP);
-        BY_NAME.add(Items.MUSIC_DISC_CREATOR);
-        BY_NAME.add(Items.MUSIC_DISC_CREATOR_MUSIC_BOX);
-        BY_NAME.add(Items.MUSIC_DISC_FAR);
-        BY_NAME.add(Items.MUSIC_DISC_MALL);
-        BY_NAME.add(Items.MUSIC_DISC_MELLOHI);
-        BY_NAME.add(Items.MUSIC_DISC_STAL);
-        BY_NAME.add(Items.MUSIC_DISC_STRAD);
-        BY_NAME.add(Items.MUSIC_DISC_WARD);
-        BY_NAME.add(Items.MUSIC_DISC_11);
-        BY_NAME.add(Items.MUSIC_DISC_WAIT);
-        BY_NAME.add(Items.MUSIC_DISC_OTHERSIDE);
-        BY_NAME.add(Items.MUSIC_DISC_RELIC);
-        BY_NAME.add(Items.MUSIC_DISC_5);
-        BY_NAME.add(Items.MUSIC_DISC_PIGSTEP);
-        BY_NAME.add(Items.MUSIC_DISC_PRECIPICE);
+        Set<Item> BY_NAME = Set.of(
+        Items.MUSIC_DISC_13
+        ,Items.MUSIC_DISC_CAT
+        ,Items.MUSIC_DISC_BLOCKS
+        ,Items.MUSIC_DISC_CHIRP
+        ,Items.MUSIC_DISC_CREATOR
+        ,Items.MUSIC_DISC_CREATOR_MUSIC_BOX
+        ,Items.MUSIC_DISC_FAR
+        ,Items.MUSIC_DISC_MALL
+        ,Items.MUSIC_DISC_MELLOHI
+        ,Items.MUSIC_DISC_STAL
+        ,Items.MUSIC_DISC_STRAD
+        ,Items.MUSIC_DISC_WARD
+        ,Items.MUSIC_DISC_11
+        ,Items.MUSIC_DISC_WAIT
+        ,Items.MUSIC_DISC_OTHERSIDE
+        ,Items.MUSIC_DISC_RELIC
+        ,Items.MUSIC_DISC_5
+        ,Items.MUSIC_DISC_PIGSTEP
+        ,Items.MUSIC_DISC_PRECIPICE);
         return BY_NAME;
     }
 }
--- src/main/java/dev/tr7zw/itemswapper/provider/RecordNameProvider.java
+++ src/main/java/dev/tr7zw/itemswapper/provider/RecordNameProvider.java
@@ -12,33 +12,31 @@ import net.minecraft.world.item.Items;
 
 public class RecordNameProvider implements NameProvider {
 
-    private Set<Item> BY_NAME;
-
     @Override
     public Set<Item> getItemHandlers() {
         // TODO late init this, so other mods music discs are loaded?
         //spotless:off
         //#if MC >= 12100
-            BY_NAME.add(Items.MUSIC_DISC_13);
-            BY_NAME.add(Items.MUSIC_DISC_CAT);
-            BY_NAME.add(Items.MUSIC_DISC_BLOCKS);
-            BY_NAME.add(Items.MUSIC_DISC_CHIRP);
-            BY_NAME.add(Items.MUSIC_DISC_CREATOR);
-            BY_NAME.add(Items.MUSIC_DISC_CREATOR_MUSIC_BOX);
-            BY_NAME.add(Items.MUSIC_DISC_FAR);
-            BY_NAME.add(Items.MUSIC_DISC_MALL);
-            BY_NAME.add(Items.MUSIC_DISC_MELLOHI);
-            BY_NAME.add(Items.MUSIC_DISC_STAL);
-            BY_NAME.add(Items.MUSIC_DISC_STRAD);
-            BY_NAME.add(Items.MUSIC_DISC_WARD);
-            BY_NAME.add(Items.MUSIC_DISC_11);
-            BY_NAME.add(Items.MUSIC_DISC_WAIT);
-            BY_NAME.add(Items.MUSIC_DISC_OTHERSIDE);
-            BY_NAME.add(Items.MUSIC_DISC_RELIC);
-            BY_NAME.add(Items.MUSIC_DISC_5);
-            BY_NAME.add(Items.MUSIC_DISC_PIGSTEP);
-            BY_NAME.add(Items.MUSIC_DISC_PRECIPICE);
-            return BY_NAME;
+            return Set.of(
+                    Items.MUSIC_DISC_13
+                    ,Items.MUSIC_DISC_CAT
+                    ,Items.MUSIC_DISC_BLOCKS
+                    ,Items.MUSIC_DISC_CHIRP
+                    ,Items.MUSIC_DISC_CREATOR
+                    ,Items.MUSIC_DISC_CREATOR_MUSIC_BOX
+                    ,Items.MUSIC_DISC_FAR
+                    ,Items.MUSIC_DISC_MALL
+                    ,Items.MUSIC_DISC_MELLOHI
+                    ,Items.MUSIC_DISC_STAL
+                    ,Items.MUSIC_DISC_STRAD
+                    ,Items.MUSIC_DISC_WARD
+                    ,Items.MUSIC_DISC_11
+                    ,Items.MUSIC_DISC_WAIT
+                    ,Items.MUSIC_DISC_OTHERSIDE
+                    ,Items.MUSIC_DISC_RELIC
+                    ,Items.MUSIC_DISC_5
+                    ,Items.MUSIC_DISC_PIGSTEP
+                    ,Items.MUSIC_DISC_PRECIPICE);
         //#else
         //$$ return ((ItemVariantAccess) (Object) Items.MUSIC_DISC_CAT).getAllItemVariants();
         //#endif
