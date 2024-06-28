fc3e1d75c1864c7de4cdbdfc3b5960ccede6143d
Wolfdv1 <dan@danw.pl>
2024-06-28 19:24:40 +0200

    Revert "Tedious update to 1.21 ResourceLocation methods, plus patch on byteBufferBuilder for RenderHelper"
    This reverts commit 7230ad7f

--- src/main/java/dev/tr7zw/itemswapper/mixin/MinecraftMixin.java
+++ src/main/java/dev/tr7zw/itemswapper/mixin/MinecraftMixin.java
@@ -79,7 +79,7 @@ public class MinecraftMixin {
         if (ConfigManager.getInstance().getConfig().pickblockOnToolsWeapons != PickBlockMode.ALLOW) {
             ItemList list = ItemSwapperSharedMod.instance.getItemGroupManager()
                     .getList(player.getMainHandItem().getItem());
-            // spotless:off
+            //spotless:off
             //#if MC >= 12100
             if (list != null && (list.getId().equals(ResourceLocation.fromNamespaceAndPath("itemswapper", "v2/weapons"))
                     || list.getId().equals(ResourceLocation.fromNamespaceAndPath("itemswapper", "v2/tools")))) {
