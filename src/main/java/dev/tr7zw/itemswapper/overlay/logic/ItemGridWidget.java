518d656d0d0b495548fd50b21c122967f1384f16
tr7zw <tr7zw@live.de>
2025-03-23 20:25:25 +0100

    Fix render

--- src/main/java/dev/tr7zw/itemswapper/overlay/logic/ItemGridWidget.java
+++ src/main/java/dev/tr7zw/itemswapper/overlay/logic/ItemGridWidget.java
@@ -57,9 +57,9 @@ public abstract class ItemGridWidget implements GuiWidget {
         if (getWidgetArea().getBackgroundTexture() == null) {
             // fallback in case of no background texture
             //#if MC >= 12002
-            graphics.blitSprite(WidgetUtil.HOTBAR_OFFHAND_LEFT_SPRITE, x, y, 0, 29, 24);
+            graphics.blitSprite(WidgetUtil.HOTBAR_OFFHAND_LEFT_SPRITE, x, y, 29, 24, 0xFFFFFFFF);
             //#else
-            //$$ graphics.blit(WidgetUtil.WIDGETS_LOCATION, x, y, 0, 0, 24, 22, 29, 24);
+            //$$ graphics.blit(WidgetUtil.WIDGETS_LOCATION, x, y, 24, 22, 29, 24, 256, 256);
             //#endif
         }
         GuiSlot guiSlot = getSlots().get(listId);
